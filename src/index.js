9d96608edc7d2b7dbb33e5e9a1848441f90e6143
krisbobo <kiyongbohbo@gmail.com>
2022-11-26 09:48:21 +0100

    Import modules and add event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import { freshGame } from './modules/new_game';
+import sendResult from './modules/send_result';
+
+const refreshBtn = document.querySelector('.refresh-btn');
+const form = document.querySelector('#form');
+
+refreshBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  freshGame.populateList();
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendResult();
+});
+
+window.onload = (e) => {
+  e.preventDefault();
+  freshGame.populateList();
+};
